da4dbef3916277576f840cb1c160375dac593904
Your Name <your@email.com>
2017-05-01 22:08:48 -0400

    added routes and functionality

--- app.js
+++ app.js
@@ -83,16 +83,8 @@ app.use('/users', users);
 app.set('port', (process.env.PORT || 3000));
 
 app.listen(app.get('port'), function(){
-	console.log('Server started on port '+app.get('port'));
+  console.log('Server started on port '+app.get('port'));
 });
-
-
-
-
-
-
-
-
 
 
 
